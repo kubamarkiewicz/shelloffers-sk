41131ea64f458a51cc402ac97b97715544f463eb
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2016-11-21 18:09:52 +0100

    fixes

--- cms/plugins/shell/offers/routes.php
+++ cms/plugins/shell/offers/routes.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::get('/api-v2', 'Shell\Offers\Classes\ApiDiscoveryController@index');
+Route::get('/api', 'Shell\Offers\Classes\ApiDiscoveryController@index');
 
-Route::get('/api-v2/offers', 'Shell\Offers\Classes\OffersController@index');
-Route::get('/api-v2/job-titles', 'Shell\Offers\Classes\JobTitlesController@index');
-Route::get('/api-v2/provinces', 'Shell\Offers\Classes\ProvincesController@index');
-Route::get('/api-v2/cities', 'Shell\Offers\Classes\CitiesController@index');
-Route::post('/api-v2/applications', 'Shell\Offers\Classes\ApplicationsController@save');
+Route::get('/api/offers', 'Shell\Offers\Classes\OffersController@index');
+Route::get('/api/job-titles', 'Shell\Offers\Classes\JobTitlesController@index');
+Route::get('/api/provinces', 'Shell\Offers\Classes\ProvincesController@index');
+Route::get('/api/cities', 'Shell\Offers\Classes\CitiesController@index');
+Route::post('/api/applications', 'Shell\Offers\Classes\ApplicationsController@save');
