bb190a9ff6a278f673f09565bfc890a7e2d61e79
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-02-22 16:45:05 -0500

    update

--- js/config/config.js
+++ js/config/config.js
@@ -8,11 +8,11 @@ window.config.lang = "en";
 window.config.api = {
 
 	"urls" : {
-		"getOffers"			: "https://shelloffers.com/sk/dev/form/cms/api/offers",
-		"getJobTitles"		: "https://shelloffers.com/sk/dev/form/cms/api/job-titles",
-		"getProvinces"		: "https://shelloffers.com/sk/dev/form/cms/api/provinces",
-		"getCities"			: "https://shelloffers.com/sk/dev/form/cms/api/cities",
-		"postApplication"	: "https://shelloffers.com/sk/dev/form/cms/api/applications"
+		"getOffers"			: "cms/api/offers",
+		"getJobTitles"		: "cms/api/job-titles",
+		"getProvinces"		: "cms/api/provinces",
+		"getCities"			: "cms/api/cities",
+		"postApplication"	: "cms/api/applications"
 	}
 
 }
