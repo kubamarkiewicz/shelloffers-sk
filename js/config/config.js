ace7f6ed67d0787a7856aa3487093ae203041db7
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-02-22 16:31:07 -0500

    new dev config

---
+++ js/config/config.js
@@ -0,0 +1,18 @@
+
+window.config = window.config || {};
+
+window.config.env = "dev"; // dev|prod
+
+window.config.lang = "en";
+
+window.config.api = {
+
+	"urls" : {
+		"getOffers"			: "https://sk.shelloffers.com/form/cms/api/offers",
+		"getJobTitles"		: "https://sk.shelloffers.com/form/cms/api/job-titles",
+		"getProvinces"		: "https://sk.shelloffers.com/form/cms/api/provinces",
+		"getCities"			: "https://sk.shelloffers.com/form/cms/api/cities",
+		"postApplication"	: "https://sk.shelloffers.com/form/cms/api/applications"
+	}
+
+}
