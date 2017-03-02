bf589ddad32c8417604f3ea35ff759848aa2948b
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-03-02 16:28:15 -0500

    translate panel

---
+++ cms/plugins/shell/offers/lang/sk/lang.php
@@ -0,0 +1,45 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Ponuky práce Shell',
+        'description' => 'Ponuky práce na čerpacích staniciach Shell',
+    ],
+    'station' => [
+        'stations' => 'Čerpacie stanice',
+        'id' => 'ID stanice',
+        'shell-name' => 'Názov Shell',
+        'email' => 'E-mail',
+        'station' => 'Stanica',
+        'post-code' => 'Poštové smerovacie číslo',
+        'city' => 'Mesto',
+        'street' => 'Ulica',
+    ],
+    'province' => [
+        'province' => 'Kraj',
+        'provinces' => 'Kraje',
+        'name' => 'Názov',
+    ],
+    'job-title' => [
+        'job-title' => 'Pracovné miesto',
+        'job-titles' => 'Pracovné miesta',
+        'name' => 'Názov pracovného miesta',
+        'description' => 'Šablóna pracovnej ponuky',
+    ],
+    'offer' => [
+        'job-offer' => 'Ponuka práce',
+        'job-offers' => 'Ponuky práce',
+        'description' => 'Opis ponuky',
+        'published' => 'Zverejnená',
+        'valid-from' => 'Platná od',
+        'valid-to' => 'Platná do',
+        'create-date' => 'Dátum vytvorenia',
+        'created-by' => 'Vytvorená:',
+    ],
+    'application' => [
+        'date' => 'Dátum',
+        'statistics' => '',
+        'statistics' => 'Štatistické údaje',
+        'user' => '',
+        'site' => 'Čerpacie stanice (len pre Manažérov čerpacích staníc)',
+    ]
+
+];
