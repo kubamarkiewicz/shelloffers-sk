562851ff536ddb531f7854883178b46ab2ce67d8
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-03-04 19:44:07 -0500

    translation fix 1

---
+++ cms/modules/system/lang/sk/client.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client-side Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | These are messages made available to the client browser via JavaScript.
+    | To compile this file run: php artisan october:util compile lang
+    |
+    */
+
+
+    'alert' => [
+        'confirm_button_text' => 'OK',
+        'cancel_button_text' => 'Storno',
+    ],
+
+];
